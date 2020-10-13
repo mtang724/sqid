dc622bca30af958fb3d231a907c0f0cb694d5f7b
EstherTMY <mtang724@gmail.com>
2020-10-12 21:18:12 -0400

    change endpoints

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -1,8 +1,8 @@
 export const commonsEndpoint = 'https://commons.wikimedia.org/w/api.php' // for image
-export const wikidataEndpoint = 'http://sitaware.isi.edu:5556/entity/'
-export const sparqlEndpoint = 'http://sitaware.isi.edu:5556/sparql/'
-export const sqidEndpoint = 'http://sitaware.isi.edu:5556/data'
-export const customDomain = 'http://sitaware.isi.edu:8051'
+export const wikidataEndpoint = 'http://localhost:5556/entity/'
+export const sparqlEndpoint = 'http://slocalhost:5556/sparql/'
+export const sqidEndpoint = 'http://localhost:5556/data'
+export const customDomain = 'http://localhost:8051'
 
 export const MAX_SIMULTANEOUS_API_REQUESTS = 25
 export const MAX_ENTITIES_PER_API_REQUEST = 50
